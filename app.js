27306d54ba93cf42ebc42ba51f51fa128c622534
ASteinheiser <me@iamandrew.io>
2017-11-14 15:46:10 -0700

    refactor a few things

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var express = require('express')
 var session = require('express-session')
 var mongoose = require('mongoose')
 
+var MONGO_URL = 'mongodb://localhost/simpleAuthentication'
+
 var app = express()
-app.use(session({ secret: 'super-secret', resave: true, saveUninitialized: false }))
+app.use(session({
+  secret: 'super-secret',
+  resave: true,
+  saveUninitialized: false,
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 var routes = require('./routes/routes.js')
 app.use('/', routes)
 
-mongoose.connect('mongodb://localhost/simpleAuthentication', { useMongoClient: true })
+mongoose.connect( MONGO_URL, { useMongoClient: true })
 var db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'))
